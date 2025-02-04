f5e8f388cec8ab363014e29be53a07beabe15fbc
cocopw <cocopw@localhost.localdomain>
2025-02-04 09:29:51 +0100

    parsing

--- __main__.py
+++ __main__.py
@@ -1,8 +1,22 @@
-import automate
-from parse import parse
+import automate;
+from parse import parse;
 file  = open("conf.aut");
 text = file.read();
 file.close()
 
 #parsing
 lines = text.split("\n")
+if lines[0]== "afd":
+    alphabet = lines[1];
+    nbetat = int(lines[2])
+    etats = []
+    for i in range(nbetat):
+        name = lines [3+i*((len(alphabet))+1)]
+        print(name)
+        arcs = []
+        for j in range(len(alphabet)):
+            li = parse('{} {} {}' ,lines[4+i*(len(alphabet)+1)+j]);
+            print (f"{li[0]}, {li[1]}, {li[2]}")
+            arcs.append(automate.arc(provenance=li[0], destination=li[2],value=li[1]))
+        etats.append(automate.etat(name=name,id = i,arcs=arcs))
+    afd = automate.afd(nbetat=nbetat,startetat= 0, alphabet= alphabet,etats=etats)
--- automate.py
+++ automate.py
@@ -1,19 +1,20 @@
-class afd:
-    def __init__(self, nbetat:int, startetat:int, alphabet:str):
-        self.nbetat = nbetat;
-        self.curr = startetat;
-        self.aphabet = alphabet;
-        self.etat = [];
-        for o in range(nbetat):
-            self.etat.append(o);
 
-class etat:
-    def __init__(self,name:str, id:int):
-        self.name = name;
-        self.id = id;
+
 
 class arc :
     def __init__(self, provenance: int, destination : int, value:str):
         self.provenance = provenance;
         self.destination = destination;
         self.value = value;
+
+class etat:
+    def __init__(self,name:str, id:int, arcs:list[arc]):
+        self.name = name;
+        self.id = id;
+        self.arcs = arcs
+class afd:
+    def __init__(self, nbetat:int, startetat:int, alphabet:str, etats:list[etat]):
+        self.nbetat = nbetat;
+        self.curr = startetat;
+        self.aphabet = alphabet;
+        self.etat = etats;
