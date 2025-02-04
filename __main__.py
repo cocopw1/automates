bf30ddd650bbd2d291491d6090ad4c0a397fd937
cocopw <cocopw@localhost.localdomain>
2025-02-04 17:35:41 +0100

    working afd

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import automate;
+import sys;
 from parse import parse;
 file  = open("conf.aut");
 text = file.read();
@@ -12,11 +13,13 @@ if lines[0]== "afd":
     etats = []
     for i in range(nbetat):
         name = lines [3+i*((len(alphabet))+1)]
-        print(name)
         arcs = []
         for j in range(len(alphabet)):
             li = parse('{} {} {}' ,lines[4+i*(len(alphabet)+1)+j]);
-            print (f"{li[0]}, {li[1]}, {li[2]}")
             arcs.append(automate.arc(provenance=li[0], destination=li[2],value=li[1]))
         etats.append(automate.etat(name=name,id = i,arcs=arcs))
     afd = automate.afd(nbetat=nbetat,startetat= 0, alphabet= alphabet,etats=etats)
+    print(sys.argv[1]);
+    for c in sys.argv[1]:
+        afd.navigate(c);
+    print(afd.__getstate__().name)
--- automate.py
+++ automate.py
@@ -3,8 +3,8 @@
 
 class arc :
     def __init__(self, provenance: int, destination : int, value:str):
-        self.provenance = provenance;
-        self.destination = destination;
+        self.provenance = int(provenance)-1;
+        self.destination = int(destination)-1;
         self.value = value;
 
 class etat:
@@ -12,9 +12,23 @@ class etat:
         self.name = name;
         self.id = id;
         self.arcs = arcs
+    def getArcfromValue(self, value: str)-> arc:
+        for arc in self.arcs:
+            if arc.value == value:
+                return arc
+        return None;
 class afd:
     def __init__(self, nbetat:int, startetat:int, alphabet:str, etats:list[etat]):
         self.nbetat = nbetat;
         self.curr = startetat;
-        self.aphabet = alphabet;
+        self.alphabet = alphabet;
         self.etat = etats;
+    def navigate(self, value:str):
+        if value in self.alphabet:
+            
+            self.curr= self.etat[self.curr].getArcfromValue(value).destination;
+            print(self.etat[self.curr].name, self.curr, self.etat[self.curr].id)
+            if self.curr == -1:
+                self = None;
+    def __getstate__(self):
+        return self.etat[self.curr]
